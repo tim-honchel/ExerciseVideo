70d0a22dd8cac576244c860ea959a65bc29c9f50
Tim Honchel <94769316+tim-honchel@users.noreply.github.com>
2024-05-03 20:17:33 -0400

    proof of concept
    created exercise table, timer buttons, and main display

---
+++ ExerciseVideo/Data/Exercise.cs
@@ -0,0 +1,9 @@
+﻿namespace ExerciseVideo.Data
+{
+    public class Exercise
+    {
+        public string Name { get; set; } = "";
+        public int Time { get; set; } = 0;
+        public int Order { get; set; }
+    }
+}
