a4dbb0a38e37d3608c7ce4e4abf5f015c7bf1369
Tim Honchel <94769316+tim-honchel@users.noreply.github.com>
2024-05-10 19:33:14 -0400

    add transition time

--- ExerciseVideo/Pages/Workout.cshtml.cs
+++ ExerciseVideo/Pages/Workout.cshtml.cs
@@ -8,30 +8,33 @@ namespace ExerciseVideo.Pages
     public class WorkoutModel : PageModel
     {
         public Exercise[]? Exercises;
+        public int TransitionTime;
         public void OnGet()
         {
-            //Exercises = new Exercise[]
-            //{
-            //    new Exercise() {Name = "Arm Swings", Time = 5, Order= 0 },
-            //    new Exercise() {Name = "Forward Arm Circles", Time = 5, Order = 1 },
-            //    new Exercise() {Name = "Reverse Arm Circles", Time = 5, Order = 2}
-            //};
-
             Exercises = new Exercise[]
             {
-                new Exercise() {Name = "Arm Swings", Time = 20, Order = 0},
-                new Exercise() {Name = "Forward Arm Circles", Time = 10, Order = 1},
-                new Exercise() {Name = "Reverse Arm Circles", Time = 10, Order = 2},
-                new Exercise() {Name = "Right Leg Circles", Time = 10, Order = 3},
-                new Exercise() {Name = "Left Leg Circles", Time = 10, Order = 4},
-                new Exercise() {Name = "Right Leg Kicks", Time = 10, Order = 5},
-                new Exercise() {Name = "Left Leg Kicks", Time = 10, Order = 6},
-                new Exercise() {Name = "Deep Sit Bend", Time = 10, Order = 7},
-                new Exercise() {Name = "Push Ups", Time = 30, Order = 8},
-                new Exercise() {Name = "Sit Ups", Time = 30, Order = 9},
-                new Exercise() {Name = "Squats", Time = 30, Order = 10},
-                new Exercise() {Name = "Jumping Jacks", Time = 30, Order = 11}
+                new Exercise() {Name = "Arm Swings", Time = 3, Order= 0 },
+                new Exercise() {Name = "Forward Arm Circles", Time = 3, Order = 1 },
+                new Exercise() {Name = "Reverse Arm Circles", Time = 3, Order = 2}
             };
+
+            //Exercises = new Exercise[]
+            //{
+            //    new Exercise() {Name = "Arm Swings", Time = 20, Order = 0},
+            //    new Exercise() {Name = "Forward Arm Circles", Time = 10, Order = 1},
+            //    new Exercise() {Name = "Reverse Arm Circles", Time = 10, Order = 2},
+            //    new Exercise() {Name = "Right Leg Circles", Time = 10, Order = 3},
+            //    new Exercise() {Name = "Left Leg Circles", Time = 10, Order = 4},
+            //    new Exercise() {Name = "Right Leg Kicks", Time = 10, Order = 5},
+            //    new Exercise() {Name = "Left Leg Kicks", Time = 10, Order = 6},
+            //    new Exercise() {Name = "Deep Sit Bend", Time = 10, Order = 7},
+            //    new Exercise() {Name = "Push Ups", Time = 30, Order = 8},
+            //    new Exercise() {Name = "Sit Ups", Time = 30, Order = 9},
+            //    new Exercise() {Name = "Squats", Time = 30, Order = 10},
+            //    new Exercise() {Name = "Jumping Jacks", Time = 30, Order = 11}
+            //};
+
+            TransitionTime = 1;
         }
     }
 }
